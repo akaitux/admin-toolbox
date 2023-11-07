8cf6128125cbe8c445cb523bfb322587006ba137
Mikhail Bocharov <bocharov.mihail@gmail.com>
2023-11-07 11:29:03 +0300

    wip

--- scripts/source_script.sh
+++ scripts/source_script.sh
@@ -6,7 +6,8 @@
 # VAULT_LOGIN_METHOD
 # VAULT_LOAD_VAR_<ENV_NAME> = <PATH_INTO_VAULT>:value
 # GITLAB_ADDR - without protocol (for example, gitlab.domain.com)
-# SSH_ENABLE_AUTOCOMPLETE_FROM_ANSIBLE
+# SSH_AUTOCMP_ANSIBLE_CACHE_ENABLED
+# SSH_AUTOCMP_ANSIBLE_CACHE_TIME
 
 ### GITLAB
 # export TG_GITLAB_USER, TG_GITLAB_PASSWORD
@@ -29,9 +30,9 @@ ssh_autocomplete () {
             hosts[$h]="0"
         done
     fi
-    if [ "$SSH_ENABLE_AUTOCOMPLETE_FROM_ANSIBLE" ]; then
+    if [ "$SSH_AUTOCMP_ANSIBLE_CACHE_TIME" ]; then
         local cache_file="$TOOLBOX_WORKDIR/.cache_hosts_autocmp"
-        local cache_seconds="600"
+        local cache_seconds="${SSH_AUTOCMP_ANSIBLE_CACHE_TIME:-600}"
         if [ -f "$cache_file" ] && [ $(stat --format=%Y $cache_file) -le $(( $(date +%s) + $cache_seconds )) ]; then
             while read h; do
                 hosts[$h]="0"
